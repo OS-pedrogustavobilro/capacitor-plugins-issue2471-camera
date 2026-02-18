154ab02a279f758405ca38149979002f3d15b3e2
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-18 15:37:11 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-plugins-issue2471-camera',
+  webDir: 'www'
+};
+
+export default config;
